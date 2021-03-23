5564191c5205c445e4b0b3658088de8cff139e03
Rafael Henrique <rafael.sousa@pravaler.com.br>
2021-03-23 09:01:18 -0300

    Rotas e resource para eventos

--- app/artbook/app.py
+++ app/artbook/app.py
@@ -6,7 +6,7 @@ from neo4j import GraphDatabase, basic_auth
 from flask import Flask, g
 from flask_restful import Api
 
-from artbook.resources.artist import Artist, Artwork, ArtistList, ArtworkList, ArtworkAuthorship, ArtistAuthorship
+from artbook.resources.artist import Artist, Artwork, ArtistList, ArtworkList, ArtworkAuthorship, ArtistAuthorship, Event, EventList
 
 
 
@@ -51,9 +51,12 @@ def create_app(test_config=None):
 
     api.add_resource(ArtworkList, '/api/artworks/', resource_class_kwargs={'db': db})
     api.add_resource(Artwork, '/api/artworks/<string:id>', resource_class_kwargs={'db': db})
-    
+
     api.add_resource(ArtworkAuthorship, '/api/artworks/<string:id>/authors/', resource_class_kwargs={'db': db})
     api.add_resource(ArtistAuthorship, '/api/artists/<string:id>/artworks/', resource_class_kwargs={'db': db})
+
+    api.add_resource(EventList, '/api/events/', resource_class_kwargs={'db': db})
+    api.add_resource(Event, '/api/events/<string:id>', resource_class_kwargs={'db': db})
 
 
     return app
