be7c6b214e4b81cc84a279a946eefec4291c6d38
Rafael Henrique <rafael@rah.com.br>
2021-03-29 00:40:00 -0300

    Remove dead code

--- app/artbook/__init__.py
+++ app/artbook/__init__.py
@@ -12,16 +12,9 @@ def create_app(test_config=None):
     load_dotenv(find_dotenv())
 
     app = Flask(__name__, instance_relative_config=True)
-    api.init_app(app)
-
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-    # a simple page that says hello
-    @app.route('/api/')
-    def hello():
-        return {"message":"Hello, World!"}
-
-
+    api.init_app(app)
     db.init_app(app)
 
     return app
