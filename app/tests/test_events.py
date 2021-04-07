803e9b031adbd93e53bdaeea87e4f22dcbeffa96
Rafael Henrique <rafael@rah.com.br>
2021-04-07 00:26:53 -0300

    Teste de eventos

--- app/tests/test_events.py
+++ app/tests/test_events.py
@@ -6,10 +6,13 @@ import uuid
 class TestEvents:
     @pytest.fixture
     def my_uuid(self):
-        return str(uuid.uuid3(uuid.NAMESPACE_DNS, 'event'))
+        """
+        Returns a controlled uuid, providing a namespace and a name
+        """
+        return str(uuid.uuid3(uuid.NAMESPACE_DNS, self.__class__.__name__))
 
 
-    def test_post_creates_new_event_with_uuid(self, my_uuid):
+    def test_post_creates_new_event(self, my_uuid):
         new_event = {"uuid":my_uuid, "title":"Event With Given UUID", "start":"2021-01-01", "end":"2021-02-01"}
         response = requests.post("http://0.0.0.0:5000/api/events/", data=new_event)
         uuid = response.json()
@@ -19,7 +22,20 @@ class TestEvents:
         assert uuid == my_uuid
 
 
-    def test_get_list_check_event_has_fields(self, my_uuid):
+    def test_get_single_return_an_event(self, my_uuid):
+        response = requests.get("http://0.0.0.0:5000/api/events/"+my_uuid)
+        response_body = response.json()
+
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+
+        assert response_body["uuid"]  == my_uuid
+        assert response_body["title"] == "Event With Given UUID"
+        assert response_body["start"] == "2021-01-01"
+        assert response_body["end"]   == "2021-02-01"
+
+
+    def test_get_return_list_of_events(self, my_uuid):
         response = requests.get("http://0.0.0.0:5000/api/events/")
         response_body = response.json()
 
@@ -32,12 +48,40 @@ class TestEvents:
         assert response_body[0]["end"] is not None
 
 
-    def test_delete_event_with_uuid(self, my_uuid):
+    def test_put_updates_an_event(self, my_uuid):
+        updated_event = {"title":"Updated Event", "start":"2022-01-01", "end":"2022-02-01"}
+        response = requests.put("http://0.0.0.0:5000/api/events/"+my_uuid, data=updated_event)
+        response_body = response.json()
+
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+        
+        assert response_body["uuid"]  == my_uuid
+        assert response_body["title"] == "Updated Event"
+        assert response_body["start"] == "2022-01-01"
+        assert response_body["end"]   == "2022-02-01"
+
+
+    def test_patch_updates_partially_an_event(self, my_uuid):
+        updated_event = {"title":"Updated Again"}
+        response = requests.patch("http://0.0.0.0:5000/api/events/"+my_uuid, data=updated_event)
+        response_body = response.json()
+
+        assert response.status_code == 200
+        assert response.headers["Content-Type"] == "application/json"
+        
+        assert response_body["uuid"]  == my_uuid
+        assert response_body["title"] == "Updated Again"
+        assert response_body["start"] == "2022-01-01"
+        assert response_body["end"]   == "2022-02-01"
+
+
+    def test_delete_removes_an_event(self, my_uuid):
         response = requests.delete("http://0.0.0.0:5000/api/events/"+my_uuid)
         assert response.status_code == 204
 
 
-    def test_post_creates_new_event_without_providing_uuid(self):
+    def test_full_crud_without_providing_uuid(self):
         """
         This test breaks the AAA (Arrange-Act-Assert) pattern intentionally!
         I just don't know how to control the generated UUID, to use the same identifier in other methods, 
